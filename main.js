87e611c91279152190f1e9aadfe58f748e2174ed
Jaldeep Acharya <Jaldeep@Jaldeeps-MBP.home>
2016-09-24 22:31:01 -0400

    Working weather search app

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
-// v3.1.0
-//Docs at http://simpleweatherjs.com
 $(document).ready(function() {
-  $.simpleWeather({
-    location: 'Austin, TX',
-    woeid: '',
-    unit: 'f',
-    success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
-  
-      $("#weather").html(html);
+  $('#submit_city').click(function() {
+    var city = $('#city').val();
+    // Using the core $.ajax() method
+    $.ajax({
+ 
+    // The URL for the request
+    url: "https://query.yahooapis.com/v1/public/yql",
+ 
+    // The data to send (will be converted to a query string)
+    data: {
+        q:"select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')",
+        format:"json",
     },
-    error: function(error) {
-      $("#weather").html('<p>'+error+'</p>');
-    }
+ 
+    // Whether this is a POST or GET request
+    type: "GET",
+ 
+    // The type of data we expect back
+    dataType : "json",
+})
+  // Code to run if the request succeeds (is done);
+  // The response is passed to the function
+  .done(function( json ) {
+    var temp = json.query.results.channel.item.condition.temp;
+    document.getElementById("weather").innerHTML = "The current temprature in " + city + " is " + temp + "°F";
+    console.log(temp);
+  })
+  // Code to run if the request fails; the raw request and
+  // status codes are passed to the function
+  .fail(function( xhr, status, errorThrown ) {
+    console.log( "Error: " + errorThrown );
+    console.log( "Status: " + status );
+    console.dir( xhr );
+  })
+  // Code to run regardless of success or failure;
+  .always(function( xhr, status ) {
+    console.log("The request is complete!");
+  });
   });
 });

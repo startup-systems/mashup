621e61026d9f9acbeb54870db5867ee085eac3f3
Utsav Vakil <utsavvakil@gmail.com>
2016-09-25 23:02:57 -0400

    Added semi colons

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
-}
+};
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -17,4 +17,4 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
-}
+};

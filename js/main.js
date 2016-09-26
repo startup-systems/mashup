7ab00e3aa4fadb0136eb4f2c7b4b6fd27235a48d
Chris M Wang <mw866@cornell.edu>
2016-09-26 19:18:33 -0400

    333

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function initMap(ipLng, ipLat){
 function main(){
   var ipLng;
   var ipLat;
-  $.getJSON("http://ip-api.com/json/?callback=?", function(data) {
+  /*$.getJSON("http://ip-api.com/json/?callback=?", function(data) {
       var table_body = "";
       $.each(data, function(k, v) {
          table_body += "<tr><td>" + k + "</td><td><b>" + v + "</b></td></tr>";
@@ -25,7 +25,32 @@ function main(){
       initMap(data.lon, data.lat);
       console.log(data.lon, data.lat);
 
+    });*/
+    $.ajax({
+      url: "http://ip-api.com/json/?callback=?",
+      type: "GET",
+      dataType : "json",
+    })
+
+  .done(function(data) {
+    var table_body = "";
+    $.each(data, function(k, v) {
+    table_body += "<tr><td>" + k + "</td><td><b>" + v + "</b></td></tr>";
     });
+    $("#whois").html(table_body);
+    initMap(data.lon, data.lat);
+    console.log(data.lon, data.lat);
+  })
+
+  .fail(function( xhr, status, errorThrown ) {
+    alert( "Sorry, there was a problem!" );
+    console.log( "Error: " + errorThrown );
+    console.log( "Status: " + status );
+    console.dir( xhr );
+  })
+
+  .always(function( xhr, status ) {
+    alert( "The request is complete!" );
+  });
+
 }
-
-

853525b903c6add8a7a2152c555bdeb9f649bb68
Osasu Eboh <osawill@gmail.com>
2016-09-26 17:48:38 -0400

    Reddit posts to tiles

--- js/app.js
+++ js/app.js
@@ -7,8 +7,21 @@ angular.module('myApp', [])
     $http.get('https://www.reddit.com/r/funny/new/.json')
       .success(function(response) {
         angular.forEach(response.data.children, function(child) {
+          //child.data.style = {"background": "url(http://www.prepbootstrap.com/Content/images/template/ResponsiveImageTiles/city1.jpg) no-repeat center top","background-size":"cover";};
           $scope.stories.push(child.data);
         });
       test = $scope.stories[0];
     });
-});
+  })
+
+  .directive('backImg', function(){
+    return function(scope, element, attrs){
+        var url = attrs.backImg;
+        element.css({
+            'background-image': 'url(' + url +')',
+            'background-size' : 'cover'
+        });
+    };
+})
+
+;

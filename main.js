f4ad323a10b91ebb8f009b441aadd44991b936e2
ichigooo <velvetland626@gmail.com>
2016-09-27 04:33:46 +0000

    add img

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
  	$("#button").click(function() {
  		var pokedex = $("#pokedex").val();
- 		var url = "https://pokeapi.co/api/v2/ability/" + pokedex;
+ 		var url = "http://pokeapi.co/api/v2/pokemon/" + pokedex;
  		$.getJSON(url, function(data) {
       pokemon = data.name;
-      generation = data.generation.name;
-      effect_entries = data.effect_entries[0].effect;
+      weight = data.weight;
+      ability = data.abilities[0].ability.name;
+      var img = document.createElement("IMG");
+      img.src = "https://pokeapi.co/media/img/" + pokedex +".png";
+      $("div.display").html("");
+      $("div.display").append(img);
       $("div.display").append("<p>" + "You got Pokemon Name: " + pokemon + "<p>");
-      $("div.display").append("<p>" + "Pokemon Generation: "+ generation + "<p>");
-      $("div.display").append("<p>" + "Pokemon Effect Entries are: " + effect_entries + "<p>");
+      $("div.display").append("<p>" + "Weight: " + weight + "<p>");
+      $("div.display").append("<p>" + pokemon + " have ability: " + ability + "<p>");
  		});
  	});
  });

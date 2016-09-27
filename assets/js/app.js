14c3250001467b7af55c41f65a231d3f2f705631
FVCproductions <hello@fvcproductions.com>
2016-09-27 16:34:02 -0400

    attempting ajax version 💀
    Signed-off-by: FVCproductions <hello@fvcproductions.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,40 @@
-$(document).ready(function() {
-    var loadRepositories = function() {
-        var url = "https://api.github.com/users/fvcproductions/repos?callback=?";
-        $.getJSON(url, function(data) {
-            var repos = data.data;
-            if (repos.length > 1) {
-                for (var repo in repos) {
-                    if (repos.hasOwnProperty(repo)) {
-                        if (repos[repo].language === null) {
-                            repos[repo].language = "";
-                        }
-                        $('.projects .github-projects').append("<div class='card'><a href='" + repos[repo].html_url + "' target='_blank'><button type='button' class='btn btn-primary-outline'>" + repos[repo].name + "</button></a><div class='card-text'><small class='text-muted'><span><i class='fa fa-star'></i>" + repos[repo].watchers + "</span><span><i class='fa fa-code-fork'></i>" + repos[repo].forks + "</span><span class='lang'><b>" + repos[repo].language + "</span></b></small><p class='card-text'>" + repos[repo].description + "</p>");
+// $(document).ready(function() {
+//     var loadRepositories = function() {
+//         var url = "https://api.github.com/users/fvcproductions/repos?callback=?";
+//         $.getJSON(url, function(data) {
+//             var repos = data.data;
+//             if (repos.length > 1) {
+//                 for (var repo in repos) {
+//                     if (repos.hasOwnProperty(repo)) {
+//                         if (repos[repo].language === null) {
+//                             repos[repo].language = "";
+//                         }
+//                         $('.projects .github-projects').append("<div class='card'><a href='" + repos[repo].html_url + "' target='_blank'><button type='button' class='btn btn-primary-outline'>" + repos[repo].name + "</button></a><div class='card-text'><small class='text-muted'><span><i class='fa fa-star'></i>" + repos[repo].watchers + "</span><span><i class='fa fa-code-fork'></i>" + repos[repo].forks + "</span><span class='lang'><b>" + repos[repo].language + "</span></b></small><p class='card-text'>" + repos[repo].description + "</p>");
+//                     }
+//                 }
+//             }
+//         });
+//     };
+//     loadRepositories();
+// });
+
+$.ajax({
+    url: "https://api.github.com/users/fvcproductions/repos?callback=?",
+    dataType: "json",
+    success: function(data) {
+        var repos = data.data;
+        if (repos.length > 1) {
+            for (var repo in repos) {
+                if (repos.hasOwnProperty(repo)) {
+                    if (repos[repo].language === null) {
+                        repos[repo].language = "";
                     }
+                    $('.projects .github-projects').append("<div class='card'><a href='" + repos[repo].html_url + "' target='_blank'><button type='button' class='btn btn-primary-outline'>" + repos[repo].name + "</button></a><div class='card-text'><small class='text-muted'><span><i class='fa fa-star'></i>" + repos[repo].watchers + "</span><span><i class='fa fa-code-fork'></i>" + repos[repo].forks + "</span><span class='lang'><b>" + repos[repo].language + "</span></b></small><p class='card-text'>" + repos[repo].description + "</p>");
                 }
             }
-        });
-    };
-    loadRepositories();
+        }
+    },
+    error: function(request, status, error) {
+        console.log(error);
+    }
 });

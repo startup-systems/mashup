3d31c2d51518d75afb2f622b33ff3822a05b8f5f
Osasu Eboh <osawill@gmail.com>
2016-09-27 19:13:55 -0400

    Switched to pure jQuery for assignment

--- js/app.js
+++ js/app.js
@@ -1,27 +1,20 @@
-test = [];
-angular.module('myApp', [])
-
-   .controller("mainCtrl", function ($http, $scope) {
-    $scope.stories = [];
-
-    $http.get('https://www.reddit.com/r/funny/new/.json')
-      .success(function(response) {
-        angular.forEach(response.data.children, function(child) {
-          //child.data.style = {"background": "url(http://www.prepbootstrap.com/Content/images/template/ResponsiveImageTiles/city1.jpg) no-repeat center top","background-size":"cover";};
-          $scope.stories.push(child.data);
-        });
-      test = $scope.stories[0];
-    });
-  })
-
-  .directive('backImg', function(){
-    return function(scope, element, attrs){
-        var url = attrs.backImg;
-        element.css({
-            'background-image': 'url(' + url +')',
-            'background-size' : 'cover'
-        });
-    };
-})
-
-;
+$.ajax('https://www.reddit.com/r/funny/new/.json', {
+    success: function(data) {
+      console.log('Got:', data);
+      $(data.data.children).each(function(idx, val){
+        //val.title
+        //val.url
+        //'//reddit.com' + val.permalink
+        elm = "<div class='col-lg-3 col-md-4 col-xs-6 thumb'>" +
+               "<a class='thumbnail' href='" + val.data.url + "'>" +
+                    "<img class='img-responsive' src='" + val.data.thumbnail + "' alt='No Image'>" +
+                "</a>" +
+              "<p>" + val.data.title + "</p>" +
+            "</div>";
+        $("#list").append(elm);
+      });
+    },
+    error: function(err) {
+      console.log('Got:', err);
+    }
+});

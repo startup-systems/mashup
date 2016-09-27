0bc156078cf41452a5bc0aac295a863a83964429
ichigooo <velvetland626@gmail.com>
2016-09-27 03:03:45 +0000

    pokeapi mashup

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+ 	$("#button").click(function() {
+ 		var pokedex = $("#pokedex").val();
+ 		var url = "http://pokeapi.co/api/v2/ability/" + pokedex;
+ 		$.getJSON(url, function(data) {
+      pokemon = data.name;
+      generation = data.generation.name;
+      effect_entries = data.effect_entries[0].effect;
+      $("div.display").append("<p>" + "You got Pokemon Name: " + pokemon + "<p>");
+      $("div.display").append("<p>" + "Pokemon Generation: "+ generation + "<p>");
+      $("div.display").append("<p>" + "Pokemon Effect Entries are: " + effect_entries + "<p>");
+ 		});
+ 	});
+ });

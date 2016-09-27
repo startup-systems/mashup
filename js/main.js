104b47e735e3f1afaad5ab9943b95c5db04085f2
Chris M Wang <mw866@cornell.edu>
2016-09-27 10:48:13 -0400

    try combine both functions into the main function.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 ///Google Map API:
 
-function initMap(ipLng, ipLat){
+/*function initMap(ipLng, ipLat){
   var map;
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: ipLat, lng: ipLng},
@@ -10,13 +10,13 @@ function initMap(ipLng, ipLat){
           position: {lat: ipLat, lng: ipLng},
           map: map
         });
-}
+}*/
 //WHOIS API:
 
 function main(){
   var ipLng;
   var ipLat;
-  /*$.getJSON("http://ip-api.com/json/?callback=?", function(data) {
+  $.getJSON("http://ip-api.com/json/?callback=?", function(data) {
       var table_body = "";
       $.each(data, function(k, v) {
          table_body += "<tr><td>" + k + "</td><td><b>" + v + "</b></td></tr>";
@@ -24,8 +24,18 @@ function main(){
       $("#whois").html(table_body);
       initMap(data.lon, data.lat);
       console.log(data.lon, data.lat);
-    });*/
-    $.ajax({
+    });
+    var map;
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: ipLat, lng: ipLng},
+      zoom: 8
+    });
+    var marker = new google.maps.Marker({
+            position: {lat: ipLat, lng: ipLng},
+            map: map
+          });
+
+/*    $.ajax({
       url: "http://ip-api.com/json/?callback=?",
       type: "GET",
       dataType : "json",
@@ -42,11 +52,12 @@ function main(){
   })
 
   .fail(function( ) {
-    alert( "Error!" );
+    alert( "Error" );
+
   })
 
-  .always(function() {
+  .always(function( xhr, status ) {
     alert( "The request is complete!" );
   });
-
+*/
 }
